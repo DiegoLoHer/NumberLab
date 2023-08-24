3394da60d73df9fe519f9bea7bbe897a3923d676
ENRIQUE AMEZCUA <jamezcua@na-at.com.mx>
2023-08-24 12:39:47 -0600

    SE AGREGAN PERMISOS PARA APLICACIÓN COMO EN INTANCE ANTERIOR

--- randominstant/src/main/java/com/na_at/fad/randomnumberlab/MainActivityInstant.java
+++ randominstant/src/main/java/com/na_at/fad/randomnumberlab/MainActivityInstant.java
@@ -23,6 +23,15 @@ public class MainActivityInstant extends AppCompatActivity {
     List<String> permissions = new ArrayList<>();
 
     private static final int ALL_PERMISSIONS_RESULT = 10;
+    private static final int ALL_PERMISSIONS_RESULT_INSTANCE = 20;
+
+
+    private static final String[] PERMISSIONSAPP = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +66,12 @@ public class MainActivityInstant extends AppCompatActivity {
             }
 
         });
+
+
+        findViewById(R.id.buttonForm).setOnClickListener(view -> {
+            startRecordVideo();
+        });
+
         // ATTENTION: This was auto-generated to handle app links.
         Intent appLinkIntent = getIntent();
         String appLinkAction = appLinkIntent.getAction();
@@ -107,10 +122,43 @@ public class MainActivityInstant extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "FALTAN PERMISOS", Toast.LENGTH_SHORT).show();
             }
+        } else if (requestCode == ALL_PERMISSIONS_RESULT_INSTANCE) {
+            if (hasPermissions(PERMISSIONSAPP)) {
+                Toast.makeText(this, "PERMISOS OTORGADOS", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "FALTA PERMISOS", Toast.LENGTH_SHORT).show();
+            }
         } else {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
+    private boolean hasPermissions(String[] permissions) {
+        if (permissions != null) {
+            for (String permission : permissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission DENEGATE: " + permission, Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 
+    public void startRecordVideo() {
+        if (isCameraPresentInPhone() && isMicrophonePresent()) {
+            ActivityCompat.requestPermissions(this, PERMISSIONSAPP, ALL_PERMISSIONS_RESULT_INSTANCE);
+        } else {
+            Toast.makeText(this, "NotHardware", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isMicrophonePresent() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
+    }
+
+    private boolean isCameraPresentInPhone() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
 }
